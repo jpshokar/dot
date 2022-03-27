351871418c1f0126e4421f1d75074ab60384e112
Jashanpreet Shokar <85253388+jpshokar@users.noreply.github.com>
2022-03-27 00:59:25 -0400

    Add files via upload

---
+++ scripts/setup/setup.sh
@@ -0,0 +1,10 @@
+
+chmod +x *.sh 
+chmod +x ../*.sh 
+echo "Permissions finished."
+mkdir ~/.environment
+git clone https://github.com/jpshokar/dot/ --depth=1 --recursive ~/.environment
+sudo make -C /home/$(whoami)/.environment/dwm clean install
+sudo make -C /home/$(whoami)/.environment/st clean install
+cp $(pwd)/xinitrc ~/.xinitrc
+echo "Finished setting up, run startx now!"
