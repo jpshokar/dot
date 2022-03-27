68a55d3757f7a8ecafff90dc69d1089fe6cd96b3
Jashanpreet Shokar <85253388+jpshokar@users.noreply.github.com>
2022-03-27 00:57:38 -0400

    Delete scripts directory

--- scripts/setup/setup.sh
+++
@@ -1,6 +0,0 @@
-mkdir ~/.environment
-git clone https://github.com/jpshokar/dot/ --depth=1 --recursive -C ~/.environment
-make -C /home/$(whoami)/.environment/dwm clean install
-make -C /home/$(whoami)/.environment/st clean install
-cp .xinitrc ~/
-echo "Finished setting up, run startx now!"
