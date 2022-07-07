b03a120215b02956d75f30bbf8e8e7bc141e46dd
jpshokar <85253388+jpshokar@users.noreply.github.com>
2022-07-06 22:43:43 -0400

    Update setup.sh

--- scripts/setup/setup.sh
+++ scripts/setup/setup.sh
@@ -1,10 +1,9 @@
-
+sudo pacman -S alacritty
 chmod +x *.sh 
 chmod +x ../*.sh 
 echo "Permissions finished."
 mkdir ~/.environment
 git clone https://github.com/jpshokar/dot/ --depth=1 --recursive ~/.environment
 sudo make -C /home/$(whoami)/.environment/dwm clean install
-sudo make -C /home/$(whoami)/.environment/st clean install
 cp $(pwd)/xinitrc ~/.xinitrc
 echo "Finished setting up, run startx now!"
